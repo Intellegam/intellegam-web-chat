62efb40c6194d61834b0d58fc3fcc67d168f6c79
Memo <mehmetxeris@gmail.com>
2025-02-21 16:21:04 +0100

    fix: font not being loaded

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth";
 
-import { authConfig } from '@/app/(auth)/auth.config';
+import { authConfig } from "@/app/(auth)/auth.config";
 
 export default NextAuth(authConfig).auth;
 
 //TODO: rework this regex
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|iframe|images).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|iframe|images|fonts).*)",
+  ],
 };
