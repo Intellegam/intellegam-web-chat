8068824fe8a3ca09cc95f745947a2472be96bc8e
Memo <mehmetxeris@gmail.com>
2025-02-24 16:46:42 +0100

    feat(Preview-Attachment): file uploads can now be removed

--- components/preview-attachment.tsx
+++ components/preview-attachment.tsx
@@ -1,19 +1,26 @@
 import type { Attachment } from 'ai';
 
+import { X } from 'lucide-react';
 import { LoaderIcon } from './icons';
 
 export const PreviewAttachment = ({
   attachment,
   isUploading = false,
+  removeAttachmentCallback,
 }: {
   attachment: Attachment;
   isUploading?: boolean;
+  removeAttachmentCallback?: () => void;
 }) => {
   const { name, url, contentType } = attachment;
 
   return (
-    <div className="flex flex-col gap-2">
+    // biome-ignore lint/nursery/noStaticElementInteractions: <explanation>
+    <div onClick={removeAttachmentCallback} className="flex flex-col gap-2">
       <div className="w-20 h-16 aspect-video bg-muted rounded-md relative flex flex-col items-center justify-center">
+        <div className="absolute inset-0 bg-black opacity-0 hover:opacity-60 transition-opacity duration-300 flex items-center justify-center">
+          <X size={24} />
+        </div>
         {contentType ? (
           contentType.startsWith('image') ? (
             // NOTE: it is recommended to use next/image for images
