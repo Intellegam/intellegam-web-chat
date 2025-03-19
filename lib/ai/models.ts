d7bc64709b084426ec4027e0ce69a5687a3af668
Atem <33186240+lichterspiel@users.noreply.github.com>
2025-03-19 16:37:08 +0100

    Sync fork (#8)
    * fix: use correct model name in onUpdateDocument function (#828)
    
    * feat: add tests (#843)
    
    * chore: bump versions (#845)
    
    * fix: check chat/vote ownership during actions (#847)
    
    * Check for isComposing
    
    * feat: add reasoning tests (#856)
    
    * feat: add artifact tests (#859)
    
    * refactor: replace isLoading with status (#861)
    
    * refactor: replace message.content with message.parts (#868)
    
    * fix: poll votes after reaching minimum message count (#869)
    
    * package updates
    
    * undo icon edits
    
    * feat (vscode): add suggested vscode extension and settings configs
    
    * fix quotes
    
    * rm small suffix, more quote fixes
    
    * version in lint action
    
    * chore: use explicit pnpm 9 version for reproducibility (#873)
    
    * feat: reference geist from google fonts (#876)
    
    ---------
    
    Co-authored-by: Adil Khalil <adilkhalil@outlook.com>
    Co-authored-by: Jeremy <git@jrmyphlmn.com>
    Co-authored-by: Shusaku Uesugi <shu.chibicode@gmail.com>
    Co-authored-by: Walter Korman <shaper@vercel.com>
    Co-authored-by: Walter Korman <lemurware@gmail.com>

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,29 +1,4 @@
-import { openai } from '@ai-sdk/openai';
-import { fireworks } from '@ai-sdk/fireworks';
-import {
-  customProvider,
-  extractReasoningMiddleware,
-  wrapLanguageModel,
-} from 'ai';
-
-export const DEFAULT_CHAT_MODEL: string = 'chat-model-small';
-
-export const myProvider = customProvider({
-  languageModels: {
-    'chat-model-small': openai('gpt-4o-mini'),
-    'chat-model-large': openai('gpt-4o'),
-    'chat-model-reasoning': wrapLanguageModel({
-      model: fireworks('accounts/fireworks/models/deepseek-r1'),
-      middleware: extractReasoningMiddleware({ tagName: 'think' }),
-    }),
-    'title-model': openai('gpt-4-turbo'),
-    'artifact-model': openai('gpt-4o-mini'),
-  },
-  imageModels: {
-    'small-model': openai.image('dall-e-2'),
-    'large-model': openai.image('dall-e-3'),
-  },
-});
+export const DEFAULT_CHAT_MODEL: string = 'chat-model';
 
 interface ChatModel {
   id: string;
@@ -33,14 +8,9 @@ interface ChatModel {
 
 export const chatModels: Array<ChatModel> = [
   {
-    id: 'chat-model-small',
-    name: 'Small model',
-    description: 'Small model for fast, lightweight tasks',
-  },
-  {
-    id: 'chat-model-large',
-    name: 'Large model',
-    description: 'Large model for complex, multi-step tasks',
+    id: 'chat-model',
+    name: 'Chat model',
+    description: 'Primary model for all-purpose chat',
   },
   {
     id: 'chat-model-reasoning',
