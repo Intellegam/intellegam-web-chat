f1791969714a0ab1129b508f7ac02a91fa7c934c
Atem <33186240+lichterspiel@users.noreply.github.com>
2025-05-28 16:26:07 +0200

    Mehmet/UI fix header and avatar (#29)
    * refactor(ui): chat-header works with sidebar
    
    * style(assistant-avatar): adjust styling of the avatar

--- components/assistant-avatar.tsx
+++ components/assistant-avatar.tsx
@@ -1,17 +1,9 @@
-import Image from 'next/image';
-
 export default function AssistantAvatar({ chatLogo }: { chatLogo?: string }) {
-  //TODO: Logo should change based on theme
+  //TODO: Logo should change based on theme -meris
   const avatarImage = chatLogo || '/images/intellegam_logo_light.svg';
   return (
     <div className="size-8 flex items-center rounded-full justify-center ring-1 shrink-0 ring-border">
-      <Image
-        className="size-6"
-        width={10}
-        height={10}
-        src={avatarImage}
-        alt="chat logo"
-      />
+      <img className="h-auto w-7" src={avatarImage} alt="chat logo" />
     </div>
   );
 }
