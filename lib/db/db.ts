4432d08102245cb76b5475b5c98028366e66f7b4
Memo <mehmetxeris@gmail.com>
2025-07-01 11:53:56 +0200

    refactor: move db access to its own file to make testing easier

---
+++ lib/db/db.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+// biome-ignore lint: Forbidden non-null assertion.
+const client = postgres(process.env.POSTGRES_URL!);
+const db = drizzle(client);
+
+export const getDB = () => db;
